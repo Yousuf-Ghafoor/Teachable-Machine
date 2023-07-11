4b380fc27f66a4660d52a58ccbf689b087290b4f
Muhammd Yousuf <mohammad30yousuf30@gmail.com>
2023-07-11 16:51:49 +0500

    muhammad yousuf

---
+++ Something New/app.js
@@ -0,0 +1,11 @@
+function getTemperature() {
+    const input = document.getElementById("input").value;
+
+    console.log(input);
+}
+
+
+function getValue() {
+    const selector = document.getElementById("selector").value;
+    console.log(selector);
+}
